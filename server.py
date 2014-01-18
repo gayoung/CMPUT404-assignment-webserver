4ceabaec9a6a4d2f44a4ab5ee3e76b64b5af9fc8
Ga Young Kim <gayoung1@cs.ualberta.ca>
2014-01-17 17:29:09 -0700

    made some minor changes to license part of assignment

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import SocketServer
 # coding: utf-8
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
+# (modified by Ga Young Kim)
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
